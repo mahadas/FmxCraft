19d24bc42761569ab2fab8ee0c02b5ef7fc68610
mahadas <54777320+mahadas@users.noreply.github.com>
2019-09-11 15:04:04 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,13 +1,11 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const config = require("./config.json");
-
 
 client.on('ready', () => {
     console.log('I am ready!');
 });
 
-var prefix = ("-");
+var prefix = ("!");
 
 
 client.on('message', message => {
