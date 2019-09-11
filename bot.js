3362c4e36391f9e8ab55be46af8d340017364c0d
mahadas <54777320+mahadas@users.noreply.github.com>
2019-09-11 14:25:28 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,22 +6,23 @@ client.on('ready', () => {
 });
 
 const prefix = "+";
-client.on ("message", (message) => {
-	if (message.author.bot) return;
-
-	if (message.content.startsWith (prefix + "hello")) {
-		message.reply ('Hi');
-	}
-    if (message.content.startsWith (prefix + 'how to embed') {
-		const embed = new RichEmbed()
-
-		.setTitle('A slick little embed')
-		.setColor(0xFF0000)
-		.setDescription('Hello, this is a slick embed!');
-		message.channel.send(embed);
-	};	
-};
-
+client.on('message', message => {
+  // If the message is "how to embed"
+  if (message.content === 'how to embed') {
+    // We can create embeds using the MessageEmbed constructor
+    // Read more about all that you can do with the constructor
+    // over at https://discord.js.org/#/docs/main/stable/class/RichEmbed
+    const embed = new RichEmbed()
+      // Set the title of the field
+      .setTitle('A slick little embed')
+      // Set the color of the embed
+      .setColor(0xFF0000)
+      // Set the main content of the embed
+      .setDescription('Hello, this is a slick embed!');
+    // Send the embed to the same channel as the message
+    message.channel.send(embed);
+  }
+});
 
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
