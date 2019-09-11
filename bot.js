474ad26ee6e7f1a7d536e1543893bf3c62b62df4
mahadas <54777320+mahadas@users.noreply.github.com>
2019-09-11 14:35:09 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,21 +1,20 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const exampleEmbed = new Discord.RichEmbed()
-	.setTitle('Some title')
-	.attachFiles(['../assets/discordjs.png'])
-	.setImage('attachment://discordjs.png');
 
 
 client.on('ready', () => {
     console.log('I am ready!');
 });
-client.on('message', message => {
-  if (command === 'estado') {
-  	message.channel.send('Que buen servidor no?')
-  	channel.send(exampleEmbed);
-  }
 
+
+client.on('message', message => {
+	if (message.content === '!ping') {
+		// send back "Pong." to the channel the message was sent in
+		message.channel.send('Pong.');
+	}
 });
 
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
+
