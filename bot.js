0af4bab96873e92e06032593d9f3b4e781b17e00
mahadas <54777320+mahadas@users.noreply.github.com>
2019-09-11 13:16:48 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,11 +5,18 @@ client.on('ready', () => {
     console.log('I am ready!');
 });
 
-client.on('message', message => {
-    if (message.content === 'ping') {
-    	message.reply('pong');
-  	}
+const prefix = "+";
+client.on ("message", (message) => {
+	if (message.author.bot) return;
+
+	if (message.content.startsWith (prefix + "hello")) {
+		message.reply ('Hi');
+	}
+
+
 });
+
 
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
+
