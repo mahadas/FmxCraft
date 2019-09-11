b1eb591823d72708be6c5d6bfd64318bf2cdc6fc
mahadas <54777320+mahadas@users.noreply.github.com>
2019-09-11 15:05:17 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -15,7 +15,12 @@ client.on('message', message => {
 	if (message.content.startsWith(prefix + "hola")) {
 	message.channel.send("Hola que tal?");
 	}
-
+	if (message.content.startsWith(prefix +"embed")){
+    message.channel.send({embed: {
+      color: 3447003,
+      description: "Esto es un simple mensaje embed."
+    }});
+	}
 });
 
 // THIS  MUST  BE  THIS  WAY
