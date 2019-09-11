1c225f721bb56708b638f4fa9de50484582d5078
mahadas <54777320+mahadas@users.noreply.github.com>
2019-09-11 13:05:05 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,10 +6,11 @@ client.on('ready', () => {
 });
 
 client.on('message', message => {
-    if (message.content === ',ip') {
-    	message.reply({file: ["http://status.mclive.eu/FmxCraft/fmxcraft.mcserv.me/25565/banner.png"]});
-  	}
-});
+    if (message.content === 'ip') {
+    	message.channel.send("Aca esta nuestra IP", {
+    file: "http://status.mclive.eu/FmxCraft/fmxcraft.mcserv.me/25565/banner.png"});
+};
 
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
+
