9ae0238f3d9e31a8e92802c041b6624489d3564f
mahadas <54777320+mahadas@users.noreply.github.com>
2019-09-11 14:40:27 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,15 +1,14 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const exampleEmbed = new Discord.RichEmbed()
-	.setTitle('Some title');
-	.setColor('#0099ff');
-	.addField('Regular field title', 'Some value here')
 
 client.on('ready', () => {
     console.log('I am ready!');
 });
 
+const exampleEmbed = new Discord.RichEmbed()
+	.setTitle('Some title')
 
+	
 client.on('message', message => {
 	if (message.content === '!ping') {
 		// send back "Pong." to the channel the message was sent in
