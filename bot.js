de5a305695623a9a37d68f2fe5fee09ed292620f
mahadas <54777320+mahadas@users.noreply.github.com>
2019-09-12 10:30:37 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -28,29 +28,8 @@ message.channel.send({embed: {
     }
   }
 });
+
 }
-         if (message.content.startsWith(prefix + "ayuda")) {
-message.channel.send({embed: {
-    color: 3447003,
-    author: {
-    },
-    title: "",
-    url: "http://google.com",
-    description: "La ip es: play.fmxcraft.ml",
-fields: [{
-        name: "Fields",
-        value: "They can have different fields with small headlines."
-      },
-    timestamp: new Date(),
-    footer: {
-      text: "© FmxCraft"
-    }
-  }
-});	 
-
-
-	}
-});
 
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
