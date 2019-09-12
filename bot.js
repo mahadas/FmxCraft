266fe50298b0bc75c914295a57aa28831be786af
mahadas <54777320+mahadas@users.noreply.github.com>
2019-09-12 08:28:37 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -18,8 +18,6 @@ client.on('message', message => {
 message.channel.send({embed: {
     color: 3447003,
     author: {
-      name: client.user.username,
-      icon_url: client.user.avatarURL
     },
     title: "",
     url: "http://google.com",
