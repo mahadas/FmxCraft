c6f406722130ec3a95b2fa5da7b2717af6bca3d9
mahadas <54777320+mahadas@users.noreply.github.com>
2019-09-12 08:25:59 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -14,37 +14,20 @@ client.on('message', message => {
 	if (message.content.startsWith(prefix + "hola")) {
 	message.channel.send("Hola que tal?");
 	}
-	if (message.content.startsWith(prefix +"embed")){
-    message.channel.send({embed: {
-      color: 3447003,
-      description: "Esto es un simple mensaje embedholaaaaaaaaaaabdfifdbejeke."
-    }});
+	if (message.content.startsWith(prefix +"ip")){
 message.channel.send({embed: {
     color: 3447003,
     author: {
       name: client.user.username,
       icon_url: client.user.avatarURL
     },
-    title: "This is an embed",
+    title: "",
     url: "http://google.com",
-    description: "This is a test embed to showcase what they look like and what they can do.",
-    fields: [{
-        name: "Fields",
-        value: "They can have different fields with small headlines."
-      },
-      {
-        name: "Masked links",
-        value: "You can put [masked links](http://google.com) inside of rich embeds."
-      },
-      {
-        name: "Markdown",
-        value: "You can put all the *usual* **__Markdown__** inside of them."
-      }
-    ],
+    description: "La ip es: play.fmxcraft.ml",
     timestamp: new Date(),
     footer: {
       icon_url: client.user.avatarURL,
-      text: "© Example"
+      text: "© FmxCraft"
     }
   }
 });
