cc4a325ace2e67a3340e2ff3b51dfc8cb5310616
mahadas <54777320+mahadas@users.noreply.github.com>
2019-09-12 08:21:40 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,9 +1,5 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const receivedEmbed = message.embeds[0];
-const exampleEmbed = new Discord.RichEmbed(receivedEmbed).setTitle('New title');
-
-
 client.on('ready', () => {
     console.log('I am ready!');
 });
@@ -23,9 +19,35 @@ client.on('message', message => {
       color: 3447003,
       description: "Esto es un simple mensaje embedholaaaaaaaaaaabdfifdbejeke."
     }});
-        if (message.content.startsWith(prefix +"estado")){
-      channel.send(exampleEmbed);
-}
+message.channel.send({embed: {
+    color: 3447003,
+    author: {
+      name: client.user.username,
+      icon_url: client.user.avatarURL
+    },
+    title: "This is an embed",
+    url: "http://google.com",
+    description: "This is a test embed to showcase what they look like and what they can do.",
+    fields: [{
+        name: "Fields",
+        value: "They can have different fields with small headlines."
+      },
+      {
+        name: "Masked links",
+        value: "You can put [masked links](http://google.com) inside of rich embeds."
+      },
+      {
+        name: "Markdown",
+        value: "You can put all the *usual* **__Markdown__** inside of them."
+      }
+    ],
+    timestamp: new Date(),
+    footer: {
+      icon_url: client.user.avatarURL,
+      text: "© Example"
+    }
+  }
+});
 }
 });
 
