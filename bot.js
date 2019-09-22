2e91e54a3a705d809b6d5de237271ee266758062
mahadas <54777320+mahadas@users.noreply.github.com>
2019-09-22 17:23:10 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,7 +2,22 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 
 client.once('ready', () => {
-	console.log('Ready!');
+	console.log('El bot ha sido iniciado correctamente.');
+});
+
+const prefix = "!"
+
+client.on('message', message => {
+	if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+	const args = message.content.slice(prefix.length).split(/ +/);
+	const command = args.shift().toLowerCase();
+
+	if (command === 'ping') {
+		message.channel.send('Aca tienes tu ping.');
+	} else if (command === 'beep') {
+		message.channel.send('Boop.');
+	}
 });
 
 client.login(process.env.BOT_TOKEN);
