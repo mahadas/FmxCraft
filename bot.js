5c356e7a15a6975b73c971a0b1af6d7b4d6b6d31
mahadas <54777320+mahadas@users.noreply.github.com>
2019-09-22 17:20:05 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,19 +1,8 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
-client.on('ready', () => {
-    console.log('Bot Iniciado Correctamente!');
+client.once('ready', () => {
+	console.log('Ready!');
 });
 
-var prefix = ("!");
-
-
-client.on('message', message => {
-	if (message.content.startsWith(prefix + "hola")) {
-	message.channel.send("Hola que tal?");
-	}
-}
-
-// THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
-
